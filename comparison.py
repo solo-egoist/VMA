d697c150cea2a44c52ed03db8dd73078321bdefb
solo-egoist <nalulatsu24@gmail.com>
2025-08-27 13:00:35 -0400

    Comparison refactoring 2 and new unit tests

--- comparison.py
+++ comparison.py
@@ -40,7 +40,7 @@ def get_matrix_row_by_indicies_sorted(matrix, row_index):
 
 def get_matrix_row_sorted(matrix, row_index):
     row = matrix[row_index]
-    return np.sort(row)[::-1]
+    return np.sort(row)[::-1][1:]
 
 def get_rows_that_contain_top_score(top_score, score_list):
     return [i for i, x in enumerate(score_list) if x == top_score]
@@ -51,8 +51,8 @@ def order_top_two_clips(matrix, top_clips):
 
     for clip in top_clips:
         sorted_row = get_matrix_row_sorted(matrix, clip)
-        if sorted_row[2] > second_highest_score_of_first_clip:
-            second_highest_score_of_first_clip = sorted_row[2]
+        if sorted_row[1] > second_highest_score_of_first_clip:
+            second_highest_score_of_first_clip = sorted_row[1]
             final_clip_index_order.append(clip)
         else:
             final_clip_index_order.insert(0, clip)
@@ -64,7 +64,7 @@ def get_first_two_clips(video_count, similarity_matrix):
 
     for i in range(video_count):
         sorted_row = get_matrix_row_sorted(similarity_matrix, i)
-        score_list.append(sorted_row[1].item())
+        score_list.append(sorted_row[0].item())
     top_score = max(score_list)
     top_two_clips = get_rows_that_contain_top_score(top_score, score_list)
 
@@ -72,16 +72,43 @@ def get_first_two_clips(video_count, similarity_matrix):
 
 def add_used_scores(used_scores, matrix, row_index):
     sorted_row = get_matrix_row_sorted(matrix, row_index)
-    for score in sorted_row[1:]:
-        used_scores.append(score)
+    used_scores.extend(sorted_row)
     return used_scores
 
 def get_score(matrix, row_index):
     sorted_row = get_matrix_row_sorted(matrix, row_index)
-    return sorted_row[2]
+    return sorted_row[1]
+
+def row_is_in_list(row_index, clip_list):
+    return row_index in clip_list
+
+def all_clips_are_sorted(final_clips_list, clip_count):
+    return len(final_clips_list) == clip_count
+
+def organize_remaining_clips(final_clips, clip_count, used_scores, matrix, next_score):
+    index = -1
+    while all_clips_are_sorted(final_clips, clip_count) == False:
+        if index >= clip_count - 1: index = -1
+        index += 1
+
+        if row_is_in_list(index, final_clips): continue
+        sorted_row = get_matrix_row_sorted(matrix, index)
+
+        if next_score not in sorted_row: continue
+
+        final_clips.append(index)
+        used_scores.append(next_score)
+        set2 = set(used_scores)
+        unique_elements = [item for item in sorted_row if item not in set2]
+
+        if unique_elements == []: continue
+
+        next_score = max(unique_elements)
+        used_scores.extend(sorted_row)
+
+    return final_clips
 
 def get_scores(video_paths):
-
     # Load model
     device = "cuda" if torch.cuda.is_available() else "cpu"
     model, preprocess = clip.load("ViT-B/32", device=device)
@@ -96,41 +123,9 @@ def get_scores(video_paths):
     used_scores = add_used_scores(used_scores, similarity_matrix, final_clips[0])
     used_scores = add_used_scores(used_scores, similarity_matrix, final_clips[1])
 
-    next_score = get_score()
+    next_score = get_score(similarity_matrix, 1)
 
-    next_score = sims[sorted_idx[2]].item()
-
-    sims = similarity_matrix[final_clips[0]]
-    sorted_idx = sims.argsort()[::-1]
-    for j in sorted_idx[1:]:
-        used_scores.append(sims[j].item())
-
-    used_scores.append(next_score)
-
-    second_final_clip_index_order = []
-
-    for x in range(len(video_paths) - 2):
-        if(len(second_final_clip_index_order) >= len(video_paths) - 2): break
-        for i, path in enumerate(video_paths):
-            if(len(second_final_clip_index_order) >= len(video_paths) - 2): break
-            if i in final_clips: continue
-            sims = similarity_matrix[i]
-            sorted_idx = sims.argsort()[::-1]
-            temp_list = []
-            for j in sorted_idx[1:]:
-                temp_list.append(sims[j].item())
-            if next_score in temp_list:
-                second_final_clip_index_order.append(i)
-                set2 = set(used_scores)
-                unique_elements = [item for item in temp_list if item not in set2]
-
-                if unique_elements:  # Check if the list is not empty
-                    next_score = max(unique_elements)
-                    used_scores.append(next_score)
-                    for j in range(len(temp_list)):
-                        used_scores.append(temp_list[j])
-
-    final_list = final_clips + second_final_clip_index_order
+    final_list = organize_remaining_clips(final_clips, len(video_paths), used_scores, similarity_matrix, next_score)
 
     print(final_list)
 
--- unittests.py
+++ unittests.py
@@ -1,43 +1,43 @@
 import unittest
 import numpy as np
-from comparison import get_first_two_clips, add_used_scores, get_score
+from comparison import get_first_two_clips, add_used_scores, get_score, organize_remaining_clips, get_matrix_row_sorted
+
+matrix_data = [
+                [0.99999976, 0.77272946, 0.7600999,  0.76695913],
+                [0.77272946, 0.9999999,  0.8072937,  0.79386276],
+                [0.7600999,  0.8072937,  1.,         0.82562786],
+                [0.76695913, 0.79386276, 0.82562786, 1.        ]
+        ]
+matrix = np.array(matrix_data, dtype=float)
+
+video_count = 4
 
 class MyFunctionTests(unittest.TestCase):
-    # def test_get_first_two_clips(self):
-    #     matrix_data = [
-    #                     [0.99999976, 0.77272946, 0.7600999,  0.76695913],
-    #                     [0.77272946, 0.9999999,  0.8072937,  0.79386276],
-    #                     [0.7600999,  0.8072937,  1.,         0.82562786],
-    #                     [0.76695913, 0.79386276, 0.82562786, 1.        ]
-    #             ]
-    #     matrix = np.array(matrix_data, dtype=float)
-    #     video_count = 4
-    #     self.assertEqual(get_first_two_clips(video_count, matrix), [3, 2])
+    def test_get_first_two_clips(self):
+        self.assertEqual(get_first_two_clips(video_count, matrix), [3, 2])
 
-    # def test_add_used_scores(self):
-    #     matrix_data = [
-    #                     [0.99999976, 0.77272946, 0.7600999,  0.76695913],
-    #                     [0.77272946, 0.9999999,  0.8072937,  0.79386276],
-    #                     [0.7600999,  0.8072937,  1.,         0.82562786],
-    #                     [0.76695913, 0.79386276, 0.82562786, 1.        ]
-    #             ]
-    #     matrix = np.array(matrix_data, dtype=float)
-    #     used_scores = []
-    #     row_index = 3
+    def test_add_used_scores(self):
+        used_scores = [0.82562786, 0.8072937, 0.7600999]
+        row_index = 3
 
-    #     self.assertEqual(add_used_scores(used_scores, matrix, row_index), [0.82562786, 0.79386276, 0.76695913])
+        self.assertEqual(add_used_scores(used_scores, matrix, row_index), [0.82562786, 0.8072937, 0.7600999, 0.82562786, 0.79386276, 0.76695913])
 
     def test_get_score(self):
-        matrix_data = [
-                        [0.99999976, 0.77272946, 0.7600999,  0.76695913],
-                        [0.77272946, 0.9999999,  0.8072937,  0.79386276],
-                        [0.7600999,  0.8072937,  1.,         0.82562786],
-                        [0.76695913, 0.79386276, 0.82562786, 1.        ]
-                ]
-        matrix = np.array(matrix_data, dtype=float)
         row_index = 3
 
         self.assertEqual(get_score(matrix, row_index), 0.79386276)
+
+    def test_organize_remaining_clips(self):
+        final_clips = [3, 2]
+        used_scores = [0.76695913, 0.79386276, 0.82562786, 0.7600999]
+        next_score = 0.8072937
+
+        self.assertEqual(organize_remaining_clips(final_clips, 4, used_scores, matrix, next_score), [3, 2, 1, 0])
+        
+    def test_get_matrix_row_sorted(self):
+        row_index = 3
+
+        np.testing.assert_array_equal(get_matrix_row_sorted(matrix, row_index), [0.82562786, 0.79386276, 0.76695913])
 
 if __name__ == '__main__':
     unittest.main()
